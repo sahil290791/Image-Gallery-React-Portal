de1e297abb87d0c124dcbc64befb8efbcb71b50d
Sahil Prajapati <sahil.prajapati@scripbox.com>
2017-11-30 07:19:27 +0530

    Add header and gallery to app

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
+import Gallery from './Gallery';
+import Header from './Header';
+
+const App = (props) => {
   return (
-    <h2 id="heading">Hello ReactJS</h2>
+    <div className="container">
+      <Header />
+      <Gallery imageList={props.imageList} />
+    </div>
   );
+};
+
+App.defaultProps = {
+  imageList: [
+    {
+      src: '../assets/images/1.jpg',
+      title: 'Image 1',
+    },
+    {
+      src: '../assets/images/2.jpg',
+      title: 'Image 2',
+    },
+    {
+      src: '../assets/images/3.jpg',
+      title: 'Image 3',
+    },
+    {
+      src: '../assets/images/4.jpg',
+      title: 'Image 4',
+    },
+  ],
+};
+
+App.propTypes = {
+  imageList: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      title: PropTypes.string,
+    }),
+  ),
 };
 
 export default App;
