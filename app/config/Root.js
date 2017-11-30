cb7fd9e8736a8e2a01622f238f11804e94a93650
Sahil Prajapati <sahil.prajapati@scripbox.com>
2017-11-30 07:19:42 +0530

    Remove react router

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from '../components/App';
 
 const Root = () => {
   return (
-    <Router>
-      <Route path="/" component={App} />
-    </Router>
+    <App />
   );
 };
 
 export default Root;
-
